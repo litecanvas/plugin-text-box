3910e7119b2c41e1d6490540ccf2c9b5f610a496
Luiz Bills <luizbills@pm.me>
2024-07-23 11:40:19 -0300

    first commit

--- README.md
+++ README.md
@@ -1,13 +1,50 @@
-# Music plugin for litecanvas
+# Text Box plugin for litecanvas
 
-An awesome plugin for [litecanvas](https://github.com/litecanvas/engine) games.
+Text boxes (dialogs, multi lines text, etc) for [litecanvas](https://github.com/litecanvas/engine) games.
 
 ## Install
 
-**NPM**: `npm i @litecanvas/plugin-name`
+**NPM**: `npm i @litecanvas/plugin-text-box`
 
-**CDN**: `https://unpkg.com/@litecanvas/plugin-name/dist/dist.js`
+**CDN**: `https://unpkg.com/@litecanvas/plugin-text-box/dist/dist.js`
 
 ## Usage
 
-lorem ipsum
+```js
+import litecanvas from "litecanvas"
+import pluginTextBox from "@litecanvas/plugin-text-box"
+
+litecanvas({
+  loop: { init, update, update },
+})
+
+use(pluginTextBox) // load the plugin
+
+function init() {
+  // The dimensions of our text box
+  box = {
+    x: 150,
+    y: 80,
+    w: WIDTH - 300,
+    h: 160,
+    textColor: 3,
+  }
+
+  message =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus placeat et itaque fuga sint, reiciendis natus sunt cumque accusamus voluptates maxime eaque labore dolorem vel quam odit similique. Dolorum numquam facilis, ex ad, in ullam porro unde excepturi, reiciendis quam voluptate amet sunt libero!"
+}
+
+function draw() {
+  cls(0)
+
+  textsize(20)
+  textbox(box.x, box.y, box.w, box.h, message, box.textColor)
+
+  // textbox() just wrap a text inside of a rectangle, does not draw any window
+  // the lines below draw a window for the textbox
+  linewidth(5)
+  rect(box.x, box.y, box.w, box.h, 3)
+}
+```
+
+Play with this demo in the [litecanvas playground]().
--- src/index.js
+++ src/index.js
@@ -1,6 +1,99 @@
-/*! Foo plugin for litecanvas v0.0.1 by Luiz Bills | MIT Licensed */
-window.pluginMusic = plugin
+/*! Text Box plugin for litecanvas v0.0.1 by Luiz Bills | MIT Licensed */
+window.pluginTextBox = plugin
 
-export default function plugin(engine, {colors, sounds, settings}) {
-    return {}
+/**
+ *
+ * @param {LitecanvasInstance} engine
+ * @returns any
+ */
+export default function plugin(engine) {
+  /**
+   * @param {number} x
+   * @param {number} y
+   * @param {number} width
+   * @param {number} height
+   * @param {string} message
+   * @param {number} color
+   */
+  function textbox(x, y, width, height, message, color = 3) {
+    const words = message.split(" ")
+    if (words.length === 0) return
+
+    if (textbox.debug) {
+      engine.push()
+      engine.linewidth(1)
+      engine.rect(x, y, width, height, 3)
+    }
+
+    const padding = ~~textbox.padding
+    const lineHeight =
+      (parseFloat(textbox.lineHeight) || 1.2) *
+      engine.textmetrics("Aqçp").height
+
+    x += padding
+    y += padding
+    width -= padding * 2
+    height -= padding * 2
+
+    const lines = _wrapText(message, width)
+    const limit = y + height
+
+    if (textbox.debug) {
+      engine.rect(x, y, width, height, 4)
+    }
+
+    for (let i = 0; i < lines.length; i++) {
+      if (y + lineHeight > limit) break
+      if (textbox.debug) {
+        engine.rect(x, y, width, lineHeight, 5)
+      }
+      engine.text(x, y, lines[i], color)
+      y += lineHeight
+      if (textbox.debug) {
+        debugger
+      }
+    }
+
+    if (textbox.debug) {
+      engine.pop()
+    }
+  }
+
+  // settings
+  /** @type {number} */
+  textbox.padding = 10
+  /** @type {number} */
+  textbox.lineHeight = 1.2
+  /** @type {boolean} */
+  textbox.debug = false
+
+  /**
+   * @param {string} textMessage
+   * @param {number} maxWidth
+   * @returns {string[]}
+   */
+  function _wrapText(textMessage, maxWidth) {
+    const words = textMessage.split(" ")
+    const lines = []
+    let line = ""
+
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i]
+      const linewidth = engine.textmetrics(line + word).width
+      if (linewidth < maxWidth) {
+        line += word + " "
+      } else {
+        lines.push(line.trim())
+        line = word + " "
+      }
+    }
+
+    if (line) {
+      lines.push(line.trim())
+    }
+
+    return lines
+  }
+
+  return { textbox }
 }
